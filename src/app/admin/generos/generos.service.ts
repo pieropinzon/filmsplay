bb62c8cc3d7c6aee3d412da40196cfa50209957a
pieropinzon <pieropinzon@gmail.com>
2017-08-27 14:58:26 -0400

    agregando la carpeta uploads

--- src/app/admin/generos/generos.service.ts
+++ src/app/admin/generos/generos.service.ts
@@ -7,8 +7,8 @@ import  'rxjs/add/operator/map';
 
 export class GenerosService{
     
-    // private url = "//localhost:4000/api/genero";
-    private url = "//pruebafilm.herokuapp.com/api/genero";
+    // private url = "//localhost:4000/api/genero"; // url de los servicios en desarrollo
+    private url = "//pruebafilm.herokuapp.com/api/genero"; // url de los servicios en produccion
     
 
     constructor(private http: Http){}
